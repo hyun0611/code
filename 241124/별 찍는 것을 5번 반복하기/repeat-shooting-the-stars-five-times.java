a96ac692f86e81a20c31b05b6bfd6ae763cd83dc
seunghyunhan <92509905+hyun0611@users.noreply.github.com>
2024-11-24 15:05:43 +0900

    Title: 별 찍는 것을 5번 반복하기, Time: 88ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 241124/별 찍는 것을 5번 반복하기/repeat-shooting-the-stars-five-times.java
@@ -0,0 +1,10 @@
+public class Main {
+    public void stars(){
+            System.out.println("**********");
+        }   
+    public static void main(String[] args) {
+        Main m = new Main();
+        for(int i=0;i<5;i++)
+            m.stars();
+        }
+}
